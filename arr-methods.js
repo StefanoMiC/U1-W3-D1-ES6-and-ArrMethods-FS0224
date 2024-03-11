cf4cdc3fb1ab698e172cab1c75f64dd8e6520b32
Stefano Miceli <mic.ninja@gmail.com>
2024-03-11 15:03:06 +0100

    adds latest changes

--- arr-methods.js
+++ arr-methods.js
@@ -15,22 +15,53 @@ fruits.forEach((fruit, i, array) => {
 });
 
 console.log(fruits);
+const numbers = [2, 7, 19, 20, 50];
+
+let sum = 0;
+numbers.forEach(num => (sum += num));
+console.log("SUM", sum);
 
 // .map() - cicla un array e richiama la funzione che gli passiamo noi, per ogni elemento dell'array
 // in più ritorna un NUOVO ARRAY con lo scopo di MODIFICARE GLI ELEMENTI di partenza
 // il map ritorna un array con la stessa length dell'originale
 
-const numbers = [2, 7, 19, 20, 50];
 const multipliedByTwo = numbers.map(num => num * 2);
 console.log(numbers);
 console.log(multipliedByTwo);
+
+const numsAsStrings = numbers.map(num => num.toString());
+console.log(numsAsStrings);
+
+const numsAsLis = numbers.map(num => `<li>${num}</li>`);
+console.log(numsAsLis);
 
 const fruitsCaps = fruits.map(fruit => fruit.toUpperCase() + "!");
 
 console.log(fruitsCaps);
 
+const arrOfObjs = [
+  { name: "Marina", surname: "Lucentini", kudos: 15 },
+  { name: "Davide", surname: "Abbadessa", kudos: 25 },
+  { name: "Emanuele", surname: "Mangano", kudos: 55 }
+];
+
+// const surnamesOnly1 = [];
+
+// for (let i = 0; i < arrOfObjs.length; i++) {
+//   const obj = arrOfObjs[i];
+
+//   surnamesOnly1.push(obj.surname);
+// }
+// console.log(surnamesOnly1); //  ['Lucentini', 'Abbadessa', 'Mangano']
+
+const surnamesOnly = arrOfObjs.map(obj => {
+  return { lastName: obj.surname, kudos: obj.kudos };
+});
+console.log(surnamesOnly); //  ['Lucentini', 'Abbadessa', 'Mangano']
+
 // .filter() - cicla un array e richiama la funzione che gli passiamo noi, per ogni elemento dell'array
-// in più ritorna un NUOVO ARRAY con lo scopo di SELEZIONARE GLI ELEMENTI di partenza
+// in più ritorna un NUOVO ARRAY con lo scopo di SELEZIONARE GLI ELEMENTI di partenza attraverso un test da superare
+// gli elementi che passeranno il test prenderanno posto nel nuovo array, gli altri verranno scartati.
 // il filter ritorna un array NON NECESSARIAMENTE con la stessa length dell'originale
 
 // const filteredFruits = fruits.filter(fruit => {
@@ -56,3 +87,10 @@ const reducedNums = numbers.reduce((accumulator, currentValue) => {
 }, 0);
 
 console.log(reducedNums);
+
+const kudosSummed = arrOfObjs.reduce((accumulator, currentValue) => {
+  console.log("ACCUMULATOR", accumulator);
+  console.log("CURR VALUE", currentValue);
+
+  return accumulator + currentValue.kudos;
+}, 0);
